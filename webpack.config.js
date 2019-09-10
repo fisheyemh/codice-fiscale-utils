d7efd0378a5feb558ad2a0e85c6370438a733923
Fernando Begio Lay <ferbegio@gmail.com>
2019-09-10 14:47:19 +0200

    Fix compilazione es6

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,20 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'codice-fiscale-utils.min.js',
